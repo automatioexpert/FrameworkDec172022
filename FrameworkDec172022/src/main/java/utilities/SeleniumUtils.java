4d36b2031419d58a6beb45b4e85c47201a7da7c3
Valmiki Vishwakarma <you@example.com>
2022-12-17 12:57:27 +0530

    Commit.

---
+++ FrameworkDec172022/src/main/java/utilities/SeleniumUtils.java
@@ -0,0 +1,14 @@
+package utilities;
+
+import org.openqa.selenium.WebDriver;
+
+public class SeleniumUtils {
+
+	private WebDriver driver;
+
+	public SeleniumUtils(WebDriver driver) {
+		this.driver = driver;
+	}
+
+	
+}
